6256d6537b7015073a31204f77b040def8dd0b25
Abyss Watcher <82021419+Abyss-W4tcher@users.noreply.github.com>
2022-10-20 13:29:56 +0200

    Update vol_install_for_apt_systems.sh

--- vol_install_for_apt_systems.sh
+++ vol_install_for_apt_systems.sh
@@ -3,15 +3,6 @@
 
 # Global vars
 
-if [ ! -z ${ZSH_VERSION+x} ]; then
-    rc_file="$HOME/.zshrc"
-elif [ ! -z ${BASH_VERSION+x} ]; then
-    rc_file="$HOME/.bashrc"
-else
-    echo "Shell is neither 'bash' nor 'zsh'. Try editing the 'rc_file' variable directly."
-    exit
-fi
-
 Help() {
     # Display Help
     echo "Volatility quick install"
@@ -24,6 +15,24 @@ Help() {
     echo "vol3_docker    Setup volatility3 docker image. Use /a/\$(readlink -f {{filename}}) for -f argument when using vol3 after install."
 }
 
+add_alias() {
+
+    check=false
+    if [ -f "$HOME/.zshrc" ]; then
+        echo "alias $1" >>$HOME/.zshrc
+        check=true
+    fi
+    if [ -f "$HOME/.bashrc" ]; then
+        echo "alias $1" >>$HOME/.bashrc
+        check=true
+    fi
+    if [ ! "$check" ]; then
+        echo "Shell is neither 'bash' nor 'zsh'."
+        exit
+    fi
+
+}
+
 check_install() {
     if [ $1 -eq 0 ]; then
         echo "Done installing $2 !"
@@ -34,9 +43,9 @@ check_install() {
 }
 
 vol2_local() {
-    if ! grep -q 'vol2=' "$rc_file"; then
-        echo 'Installing volatility2 from https://github.com/volatilityfoundation/volatility.git...'
-        command='
+
+    echo 'Installing volatility2 from https://github.com/volatilityfoundation/volatility.git...'
+    command='
         sudo apt-get install build-essential autoconf dwarfdump git subversion pcregrep libpcre++-dev python2-dev -y &&
         wget https://bootstrap.pypa.io/pip/2.7/get-pip.py -P /tmp &&
         python2 /tmp/get-pip.py &&
@@ -44,57 +53,49 @@ vol2_local() {
         pip2 install setuptools --upgrade &&
         pip2 install distorm3 yara-python pycryptodome pillow openpyxl ujson &&
         sudo git clone https://github.com/volatilityfoundation/volatility.git /opt/volatility &&
-        sudo chown -R $USER:$USER /opt/volatility/ &&
-        echo "alias vol2='"'"'python2 /opt/volatility/vol.py'"'"'" >>'"$rc_file"
-        eval $command
-        check_install $? 'local volatility2'
-    else
-        echo "ERROR : volatility2 seems already installed on the system (presence of alias in $rc_file)"
-    fi
+        sudo chown -R $USER:$USER /opt/volatility/'
+    eval $command
+    check_install $? 'local volatility2'
+    add_alias "vol2='python2 /opt/volatility/vol.py'"
+
 }
 
 vol3_local() {
-    if ! grep -q 'vol3=' "$rc_file"; then
-        echo 'Installing volatility3 from https://github.com/volatilityfoundation/volatility3.git...'
-        command='
+
+    echo 'Installing volatility3 from https://github.com/volatilityfoundation/volatility3.git...'
+    command='
         sudo apt-get install python3 python3-pip libsnappy-dev -y &&
         sudo git clone https://github.com/volatilityfoundation/volatility3.git /opt/volatility3 &&
         sudo chown -R $USER:$USER /opt/volatility3/ &&
-        pip3 install -r /opt/volatility3/requirements.txt &&
-        echo "alias vol3='"'"'python3 /opt/volatility3/vol.py'"'"'" >>'"$rc_file"
-        eval $command
-        check_install $? 'local volatility3'
-    else
-        echo "ERROR : volatility3 seems already installed on the system (presence of alias in $rc_file)"
-    fi
+        pip3 install -r /opt/volatility3/requirements.txt'
+    eval $command
+    check_install $? 'local volatility3'
+    add_alias "vol3='python3 /opt/volatility3/vol.py'"
+
 }
 
 vol2_docker() {
-    if ! grep -q 'vol2d=' "$rc_file"; then
-        echo 'Setup volatility2 docker from https://hub.docker.com/r/sk4la/volatility...'
-        command='
+
+    echo 'Setup volatility2 docker from https://hub.docker.com/r/sk4la/volatility...'
+    command='
         sudo apt-get install docker.io -y &&
-        sudo docker pull sk4la/volatility &&
-        echo "alias vol2d='"'"'docker run --rm -v /:/a sk4la/volatility'"'"'" >>'"$rc_file"
-        eval $command
-        check_install $? 'docker volatility2'
-    else
-        echo "ERROR : volatility2 docker seems already present (presence of alias in $rc_file). Fetch latest with : \"docker pull sk4la/volatility\""
-    fi
+        sudo docker pull sk4la/volatility'
+    eval $command
+    check_install $? 'docker volatility2'
+    add_alias "vol2d='docker run --rm -v /:/a sk4la/volatility'"
+
 }
 
 vol3_docker() {
-    if ! grep -q 'vol3d=' "$rc_file"; then
-        echo 'Setup volatility3 docker from https://hub.docker.com/r/sk4la/volatility...'
-        command='
+
+    echo 'Setup volatility3 docker from https://hub.docker.com/r/sk4la/volatility...'
+    command='
         sudo apt-get install docker.io -y &&
-        sudo docker pull sk4la/volatility3 &&
-        echo "alias vol3d='"'"'docker run --rm -v /:/a sk4la/volatility3'"'"'" >>'"$rc_file"
-        eval $command
-        check_install $? 'docker volatility3'
-    else
-        echo "ERROR : volatility3 docker seems already present (presence of alias in $rc_file). Fetch latest with : \"docker pull sk4la/volatility3\""
-    fi
+        sudo docker pull sk4la/volatility3'
+    eval $command
+    check_install $? 'docker volatility3'
+    add_alias "vol3d='docker run --rm -v /:/a sk4la/volatility3'"
+
 }
 
 messages=()
@@ -127,7 +128,7 @@ if ((${#messages[@]})); then
         echo -e "$message"
     done
     echo -e "\n-----------------------------------\n"
-    echo "You can now run \"source $rc_file\" to reload the shell and get the commands ready !"
+    echo "You can now run \"exec \$SHELL\" to reload the shell and get the commands ready !"
 else
     Help
 fi
