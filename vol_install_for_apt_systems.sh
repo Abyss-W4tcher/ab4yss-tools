aa9bacf59ee7a5ed5e2156e10689d3f5d989305e
Abyss Watcher <82021419+Abyss-W4tcher@users.noreply.github.com>
2023-01-18 17:15:14 +0100

    Remove most sudo usage, specify user directly
    Script can be run as root directly, which simplifies some aspects

--- vol_install_for_apt_systems.sh
+++ vol_install_for_apt_systems.sh
@@ -5,25 +5,26 @@
 
 Help() {
     # Display Help
-    echo "Volatility quick install"
-    echo
-    echo "Syntax: vol_install.sh [option(s)]"
+    echo ">>> Volatility easy install <<<"
+    echo "Need to be run as root, for dependencies installation. Additional packages (sudo, wget...) may be also installed."
+    echo "Syntax: vol_install.sh VOL_USER [option(s)]"
+    echo "Specify the user which will be using volatility as first argument. For docker usage, 'docker' group needs to be part of the 'sudo' group (or run the container as root)."
     echo "options:"
     echo "vol2_local     Install latest volatility2 github master on the system"
     echo "vol3_local     Install latest volatility3 github master on the system"
-    echo "vol2_docker    Setup volatility2 docker image. Use /a/\$(readlink -f {{filename}}) for -f argument when using vol2 after install."
-    echo "vol3_docker    Setup volatility3 docker image. Use /a/\$(readlink -f {{filename}}) for -f argument when using vol3 after install."
+    echo "vol2_docker    Setup volatility2 docker image. Use '/a/\$(readlink -f {{dumpfile}})' for volatility -f argument when using vol2 after install."
+    echo "vol3_docker    Setup volatility3 docker image. Use '/a/\$(readlink -f {{dumpfile}})' for volatility -f argument when using vol3 after install."
 }
 
 add_alias() {
 
     check=false
-    if [ -f "$HOME/.zshrc" ]; then
-        echo "alias $1" >>$HOME/.zshrc
+    if [ -f "$VOL_USER_HOME/.zshrc" ]; then
+        echo "alias $1" >>$VOL_USER_HOME/.zshrc
         check=true
     fi
-    if [ -f "$HOME/.bashrc" ]; then
-        echo "alias $1" >>$HOME/.bashrc
+    if [ -f "$VOL_USER_HOME/.bashrc" ]; then
+        echo "alias $1" >>$VOL_USER_HOME/.bashrc
         check=true
     fi
     if [ ! "$check" ]; then
@@ -48,19 +49,19 @@ vol2_local() {
 
     echo 'Installing volatility2 from https://github.com/volatilityfoundation/volatility.git...'
     command='
-        sudo apt install build-essential autoconf dwarfdump git subversion pcregrep -y &&
-        sudo apt install libpcre++-dev -y || sudo apt install libpcre3-dev -y &&
-        sudo apt install python2-dev -y || sudo apt install python-dev -y && 
+        apt install sudo build-essential autoconf dwarfdump git subversion pcregrep wget -y &&
+        apt install libpcre++-dev -y || apt install libpcre3-dev -y &&
+        apt install python2-dev -y || apt install python-dev -y && 
         wget https://bootstrap.pypa.io/pip/2.7/get-pip.py -P /tmp &&
         python2 /tmp/get-pip.py &&
         rm /tmp/get-pip.py &&
-        pip2 install setuptools --upgrade &&
-        pip2 install distorm3 yara-python pycryptodome pillow openpyxl ujson &&
-        sudo git clone https://github.com/volatilityfoundation/volatility.git /opt/volatility &&
-        sudo chown -R $USER:$USER /opt/volatility/'
+        sudo -u $VOL_USER pip2 install setuptools --upgrade &&
+        sudo -u $VOL_USER pip2 install distorm3 yara-python pycryptodome pillow openpyxl ujson &&
+        git clone https://github.com/volatilityfoundation/volatility.git /opt/volatility2 &&
+        chown -R $VOL_USER:$VOL_USER /opt/volatility2/'
     eval $command
     check_install $? 'local volatility2'
-    add_alias "vol2='python2 /opt/volatility/vol.py'"
+    add_alias "vol2='python2 /opt/volatility2/vol.py'"
 
 }
 
@@ -68,10 +69,10 @@ vol3_local() {
 
     echo 'Installing volatility3 from https://github.com/volatilityfoundation/volatility3.git...'
     command='
-        sudo apt install python3 python3-pip libsnappy-dev -y &&
-        sudo git clone https://github.com/volatilityfoundation/volatility3.git /opt/volatility3 &&
-        sudo chown -R $USER:$USER /opt/volatility3/ &&
-        pip3 install -r /opt/volatility3/requirements.txt'
+        apt install python3 python3-pip libsnappy-dev git sudo -y &&
+        git clone https://github.com/volatilityfoundation/volatility3.git /opt/volatility3 &&
+        chown -R $VOL_USER:$VOL_USER /opt/volatility3/ &&
+        sudo -u $VOL_USER pip3 install -r /opt/volatility3/requirements.txt'
     eval $command
     check_install $? 'local volatility3'
     add_alias "vol3='python3 /opt/volatility3/vol.py'"
@@ -82,8 +83,8 @@ vol2_docker() {
 
     echo 'Setup volatility2 docker from https://hub.docker.com/r/sk4la/volatility...'
     command='
-        sudo apt install docker.io -y &&
-        sudo docker pull sk4la/volatility'
+        apt install docker.io -y &&
+        docker pull sk4la/volatility'
     eval $command
     check_install $? 'docker volatility2'
     add_alias "vol2d='docker run --rm -v /:/a sk4la/volatility'"
@@ -94,16 +95,29 @@ vol3_docker() {
 
     echo 'Setup volatility3 docker from https://hub.docker.com/r/sk4la/volatility...'
     command='
-        sudo apt install docker.io -y &&
-        sudo docker pull sk4la/volatility3'
+        apt install docker.io -y &&
+        docker pull sk4la/volatility3'
     eval $command
     check_install $? 'docker volatility3'
     add_alias "vol3d='docker run --rm -v /:/a sk4la/volatility3'"
 
 }
 
+if [ "$1" == "help" ] || [ "$1" == "" ]; then
+    Help
+    exit
+fi
+
+if ! id "$1"  > /dev/null 2>&1; then
+        echo "User \"$1\" does not exist."
+        exit
+fi
+
+VOL_USER=$1
+VOL_USER_HOME="/home/$1"
+
 messages=()
-for arg; do
+for arg in "${@:2}"; do
     if [ "$arg" == "help" ]; then
         Help
         exit
